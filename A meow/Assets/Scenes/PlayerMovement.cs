751865faa6271e135675f43c4a03b99caefc6245
Khanatee <jonasskecionis@gmail.com>
2023-04-28 15:33:16 +0300

    s

--- A meow/Assets/Scenes/PlayerMovement.cs
+++ A meow/Assets/Scenes/PlayerMovement.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed;
+    public Rigitbody2D Rigitbody2D;
+
     // Start is called before the first frame update
     void Start()
     {
